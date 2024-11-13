c748737e6cd62ead31707489a323e6690b0beaf7
jackd <jackd@Jacks_PC.attlocal.net>
2024-11-12 21:49:32 -0500

    Implementation of program. Null pointer issue

--- OOP_Assignment_Fourteen/src/application/Main.java
+++ OOP_Assignment_Fourteen/src/application/Main.java
@@ -6,14 +6,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException{
 		Parent root = FXMLLoader.load(getClass().getResource("Format.fxml"));
-		Scene scene = new Scene(root, 600, 600);
-		primaryStage.setTitle("FXML Welcome");
+		Scene scene = new Scene(root, 750, 500);
+		primaryStage.setTitle("Unit Converter");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
