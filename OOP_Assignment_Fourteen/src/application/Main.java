16bec276ca6ac53b865760ec5f22315b40d8862d
jackd <jackd@Jacks_PC.attlocal.net>
2024-11-27 17:47:58 -0500

    Fixed null pointer issue

--- OOP_Assignment_Fourteen/src/application/Main.java
+++ OOP_Assignment_Fourteen/src/application/Main.java
@@ -1,12 +1,10 @@
 package application;
 	
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
